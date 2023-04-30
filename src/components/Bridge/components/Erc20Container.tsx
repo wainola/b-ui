4468900fd3ea5bff2851cb3a606e29fd75bf0ed7
Nicolas Riquelme <nrriquelme@gmail.com>
2023-04-30 18:09:16 -0400

    moving functions to util file

--- src/components/Bridge/components/Erc20Container.tsx
+++ src/components/Bridge/components/Erc20Container.tsx
@@ -1,5 +1,13 @@
-import { ERC20, FeeHandlerRouter, FeeHandlerRouter__factory } from "@buildwithsygma/sygma-contracts";
-import { ContractTransactionResponse, ethers } from "ethers";
+import {
+  ERC20,
+  FeeHandlerRouter,
+  FeeHandlerRouter__factory,
+} from "@buildwithsygma/sygma-contracts";
+import {
+  ContractTransactionResponse,
+  ethers,
+  TransactionReceipt,
+} from "ethers";
 import {
   Accessor,
   createEffect,
@@ -10,13 +18,14 @@ import {
 import { resourceIdtoChainId } from "../../../resourceIdToChainId";
 import { Domain } from "../../../types";
 import { ConnectedResource } from "../Bridge";
+import { fetchContractNameAndBalance, approveTheBridge, preparedDepositDataWithoutFee, fetchFeeHandlerAddress } from "../utils";
 
 export default function Erc20Container({
   resource,
   signer,
   bridge,
   domains,
-  chainId
+  chainId,
 }: {
   resource: ConnectedResource;
   signer: Accessor<ethers.Signer>;
@@ -36,37 +45,15 @@ export default function Erc20Container({
   const fethcContractName = async (): Promise<{
     name: string;
     balance: string;
-  }> => {
-    const { contract, decimals } = resource;
-    const currentSigner = signer();
-    console.log(await currentSigner.getAddress());
-    const connectedContract = contract.connect(currentSigner);
-    const contractName = await connectedContract.name();
-    const balance = ethers.formatUnits(
-      await connectedContract.balanceOf(await currentSigner.getAddress()),
-      decimals,
-    );
-    return { name: contractName, balance };
-  };
+  }> => fetchContractNameAndBalance(resource, signer);
 
-  const approveToTheBridgeFunc = async (amount: string) => {
-    const { contract } = resource;
-    const currentSigner = signer();
-    const connectedContract = contract.connect(currentSigner) as ERC20;
-    const approve = (await connectedContract.approve(
-      bridge,
-      ethers.parseUnits(amount, resource.decimals),
-    )) as ContractTransactionResponse;
-    const approveReceipt = await approve.wait();
-
-    return approveReceipt?.status;
-  };
+  const approve = async (
+    amount: string,
+  ): Promise<number> =>
+    approveTheBridge(amount, resource, signer, bridge);
 
   const [contractName] = createResource(fethcContractName);
-  const [approveToTheBridge] = createResource(
-    amountToApprove,
-    approveToTheBridgeFunc,
-  );
+  const [approveToTheBridge] = createResource(amountToApprove, approve);
 
   const handleSubmit = (e: Event) => {
     e.preventDefault();
@@ -76,55 +63,42 @@ export default function Erc20Container({
       amount,
     );
     setAmountToApprove(amount);
-
-    // first approve to the bridge
-
-    // then deposit
-  };
-
-  const preparedDepositDataWithoutFee = async (): Promise<[string, string, string]> => {
-    const destinationId = destination();
-    const resourceId = resource.resourceId;
-    const amount = ethers.parseUnits(amountToDeposit() as string, resource.decimals);
-
-    const signerAddress = await signer().getAddress();
-    // this is for the encoding
-    const addressToUin8Array = ethers.toBeArray(signerAddress);
-    const parsedAmount = ethers.zeroPadValue(ethers.toBeHex(amount), 32)
-    const lenSender = ethers.zeroPadValue(ethers.toBeHex(BigInt(addressToUin8Array.length)), 32)
-    const depositData = ethers.concat([parsedAmount, lenSender, addressToUin8Array])
-    const despositDataHex = ethers.hexlify(depositData)
-
-    return [despositDataHex, destinationId as string, resourceId]
   };
 
   const prepareDepositData = async () => {
-    const [encodedDepositData, destinationId, resourceId] = await preparedDepositDataWithoutFee()
+    const [encodedDepositData, destinationId, resourceId] =
+      await preparedDepositDataWithoutFee(
+        destination,
+        resource,
+        amountToDeposit,
+        signer,
+      );
 
-    const currentDomainId = resourceIdtoChainId.find(elem => elem.chainId === chainId())
+    const currentDomainId = resourceIdtoChainId.find(
+      (elem) => elem.chainId === chainId(),
+    );
 
-    const feeRouterInstance = FeeHandlerRouter__factory.connect(
-      (domains() as { domains: Domain[] }).domains.find(domain => domain.id === Number(currentDomainId?.id))?.feeRouter as string,
-      signer()
-    ) as FeeHandlerRouter
-
-    const feeHandlerAddress = await feeRouterInstance._domainResourceIDToFeeHandlerAddress(
-      destinationId,
-      resourceId
+    const feeHandlerAddress = await fetchFeeHandlerAddress(
+      domains,
+      signer,
+      currentDomainId!,
+      resourceId,
+      destinationId
     )
-    console.log("🚀 ~ file: Erc20Container.tsx:115 ~ prepareDepositData ~ feeHandlerAddress:", feeHandlerAddress)
-  }
+
+    console.log(
+      "🚀 ~ file: Erc20Container.tsx:115 ~ prepareDepositData ~ feeHandlerAddress:",
+      feeHandlerAddress,
+    );
+  };
 
   createEffect(() => {
     console.log("approveToTheBridge", approveToTheBridge());
 
-    if(approveToTheBridge()){
-      prepareDepositData()
+    if (approveToTheBridge()) {
+      prepareDepositData();
     }
   }, approveToTheBridge());
-
-
-
 
   return (
     <div>
--- src/components/Bridge/utils.ts
+++ src/components/Bridge/utils.ts
@@ -1,5 +1,5 @@
-import { ERC20, ERC20__factory } from "@buildwithsygma/sygma-contracts";
-import { ethers } from "ethers";
+import { ERC20, ERC20__factory, FeeHandlerRouter, FeeHandlerRouter__factory } from "@buildwithsygma/sygma-contracts";
+import { ContractTransactionResponse, ethers, TransactionReceipt } from "ethers";
 import { Accessor, Setter } from "solid-js";
 import { Domain } from "../../types";
 import { ConnectedResource, ConnectedResources } from "./Bridge";
@@ -29,4 +29,83 @@ export const resolveConnectionToResources = (resource: Domain['resources'][0], p
 const connectErc20Contract = (address: string, provider: ethers.BrowserProvider): ERC20 => {
   const Erc20Contract = ERC20__factory.connect(address, provider)
   return Erc20Contract
+}
+
+export const fetchContractNameAndBalance = async (resource: ConnectedResource, signer: Accessor<ethers.Signer>): Promise<{
+  name: string;
+  balance: string;
+}> => {
+  const { contract, decimals } = resource;
+  const currentSigner = signer();
+  const connectedContract = contract.connect(currentSigner);
+  const contractName = await connectedContract.name();
+  const balance = ethers.formatUnits(
+    await connectedContract.balanceOf(await currentSigner.getAddress()),
+    decimals,
+  );
+  return { name: contractName, balance };
+};
+
+export const approveTheBridge = async (amount: string, resource: ConnectedResource, signer: Accessor<ethers.Signer>, bridge: string): Promise<number> => {
+  const { contract } = resource;
+  const currentSigner = signer();
+  const connectedContract = contract.connect(currentSigner) as ERC20;
+  const approve = (await connectedContract.approve(
+    bridge,
+    ethers.parseUnits(amount, resource.decimals),
+  )) as ContractTransactionResponse;
+  const approveReceipt = await approve.wait();
+
+  return approveReceipt?.status as number;
+};
+
+export const preparedDepositDataWithoutFee = async (
+  destination: Accessor<string | null>,
+  resource: ConnectedResource,
+  amountToDeposit: Accessor<string | null>,
+  signer: Accessor<ethers.Signer>,
+): Promise<
+  [string, string, string]
+> => {
+  const destinationId = destination();
+  const resourceId = resource.resourceId;
+  const amount = ethers.parseUnits(
+    amountToDeposit() as string,
+    resource.decimals,
+  );
+
+  const signerAddress = await signer().getAddress();
+  // this is for the encoding
+  const addressToUin8Array = ethers.toBeArray(signerAddress);
+  const parsedAmount = ethers.zeroPadValue(ethers.toBeHex(amount), 32);
+  const lenSender = ethers.zeroPadValue(
+    ethers.toBeHex(BigInt(addressToUin8Array.length)),
+    32,
+  );
+  const depositData = ethers.concat([
+    parsedAmount,
+    lenSender,
+    addressToUin8Array,
+  ]);
+  const despositDataHex = ethers.hexlify(depositData);
+
+  return [despositDataHex, destinationId as string, resourceId];
+};
+
+export const fetchFeeHandlerAddress = async (domains: Accessor<{ domains: Domain[] } | [] >, signer: Accessor<ethers.Signer>, currentDomainId: { id: number, chainId: number }, resourceId: string, destinationId: string): Promise<string> => {
+
+  const feeRouterInstance = FeeHandlerRouter__factory.connect(
+    (domains() as { domains: Domain[] }).domains.find(
+      (domain) => domain.id === Number(currentDomainId?.id),
+    )?.feeRouter as string,
+    signer(),
+  ) as FeeHandlerRouter;
+
+  const feeHandlerAddress =
+    await feeRouterInstance._domainResourceIDToFeeHandlerAddress(
+      destinationId,
+      resourceId,
+    );
+
+  return feeHandlerAddress
 }
